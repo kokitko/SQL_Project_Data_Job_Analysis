5437479f03f3e2c6f2c5a64a1e2569dc8fc0b613
kokitko <koks.vladislav277@gmail.com>
2024-11-07 23:49:10 +0100

    update sql files

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -5,13 +5,11 @@ Question: What are the top-paying software engineer jobs?
 */
 
 SELECT
-    job_id,
     job_title,
     job_location,
-    job_schedule_type,
+    name AS company_name,
     salary_year_avg,
-    job_posted_date,
-    name AS company_name
+    job_posted_date
 FROM
     job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
